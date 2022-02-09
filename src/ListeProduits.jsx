9e5d47cb23ea96151a00c5924364a9c9f36fda69
Remi Gervais <e2095138@cmaisonneuve.qc.ca>
2022-02-09 14:59:11 -0500

    Ajout de sass, map et fonction flechees

--- src/ListeProduits.jsx
+++ src/ListeProduits.jsx
@@ -1,20 +1,37 @@
-import './ListeProduits.css';
+import './ListeProduits.scss';
+import Produit from './Produit';
+import lesProduits from './data/produits.json'
 
 export default function listeProduits(){
+  // Exemple avec la fonction map
+  // let notes = [59.6556, 78.26, 85.264, 35.9595];
+
+  // Pour transformer ce tableau dans un tableau qui contient les notes arrondies en une place decimale
+  // let notesArrondies = notes.map(function(uneNote){
+  //   return uneNote.toFixed(1) - 0;
+  // });
+
+  // Remarquez l'utilisation des fonctions flechees et la methode map
+  // let notesArrondies = notes.map(uneNote => uneNote.toFixed(1) - 0);
+
+  // console.log('Le tableau aux valeurs arrondies ', notesArrondies);
+
+    // // console.log('La viariable lesProduits : ', lesProduits);
+
+    // // Methode 1 (programmation useImperativeHandle, avec une boucle for)
+    // let composantsProduits = [];
+    // // Parcourir le tableau lesProduits et gener un composant Prooduit pour chaque elemnet
+    // for(let i = 0; i < lesProduits.length; i++){
+    //   composantsProduits.push(<Produit nom={lesProduits[i].nom} prix={lesProduits[i].prix} pid={lesProduits[i].pid}/>);
+    // }
+
     return(
-        <section className="produits">
+        <section className="ListeProduits">
         <h2>Nos produits</h2>
-        <div>
-          <article>
-                <img src="" alt="" />
-                <div className="titre">Titre du produit</div>
-                <div className="prix">13.95</div>
-                <button>Ajouter au panier</button>
-          </article>
-          <article>Produit 2</article>
-          <article>Produit 3</article>
-          <article>Produit 4</article>
-          <article>Produit 5</article>
+        <div className="produits">
+          {
+            lesProduits.map(p => <Produit key={p.id} nom={p.nom} prix={p.prix} pid={p.id}/>)
+          }
         </div>
       </section>
     )
--- src/Produit.jsx
+++ src/Produit.jsx
@@ -1,6 +1,11 @@
-import './Produit.css';
-export default function Produit(){
-return (
-    <div></div>
-)
+import './Produit.scss';
+export default function Produit(props){
+    return (
+        <article>
+            <img src={"images-produits/" + props.pid + ".webp"} alt={props.nom} />
+            <div className="titre">{props.nom}</div>
+            <div className="prix">{props.prix}</div>
+            <button>Ajouter au panier</button>
+        </article>
+    );
 }
